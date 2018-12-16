7b6eda2bddde262524825faa1e690613c5b297d3
mohamed.morsy <mohamed.morsy@asset.com.eg>
2018-12-16 08:46:30 +0200

    init commit

---
+++ src/main/java/com/mohamed265/garts/Main.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.mohamed265.garts;
+
+/**
+ * @author Mohamed265
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello GA-RTs");
+	}
+
+}
